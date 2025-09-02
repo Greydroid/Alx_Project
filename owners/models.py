b5cb0a866d10164ad6f11c81ee8ea34da562cacc
Greydroid <heleneelegalam@gmail.com>
2025-09-02 16:37:32 +0100

    Vettrack app project

---
+++ owners/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Owner(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
